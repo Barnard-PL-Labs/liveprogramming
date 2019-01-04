40de28537c17f69bc0f7614ec349a32faef56d4d
Mark Santolucito <mark.santolucito@yale.edu>
2019-01-03 22:53:32 -0500

    include input example characters as constants in sygus as well

--- tests/addition.js
+++ tests/addition.js
@@ -1,11 +1,4 @@
 function greet(greeting, name) {
-//  return greeting + name;
+  return greeting + name;
   return greeting + ' ' + name;
-}
-
-function format(x) {
-//////  return x;
-////  return 'h' + '3' + 'l' + 'l' + 'o';
-//  return x.replace('e', '3');
-  return x.replace('e', '3') + '!';
 }

b144f9b6e61b37f1a1fc42a50a7856616a0cf383
Bill Hallahan <billhallahan@juno.com>
2019-01-27 15:50:42 -0500

    Fixed a glitch which prevented numbers from being used in synthesis

--- tests/addition.js
+++ tests/addition.js
@@ -1,13 +1,11 @@
 function yo(x, y) {
-  return x + 5;
+ return x + 4;
 }
 
 function double(z){
-//  return z;
-  return z + z + z;
+  return z + z;
 }
 
 function concat(s1, s2){
-//  return s1 + s2
   return s1 + ' ' + s2;
 }
